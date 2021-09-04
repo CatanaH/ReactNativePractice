cacdcd340f345fd2887a0ddee9be5ed731ee5a2a
CatanaH <catanaharrington@Hotmail.com>
2021-09-03 19:34:33 -0700

    added logic for persistent memory w/AsyncStorage

--- App.js
+++ App.js
@@ -1,32 +1,32 @@
 import 'react-native-gesture-handler';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen';
 import SettingsScreen from './src/screens/SettingsScreen';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  const [minTemp, setMinTemp] = useState(25);
+  const [maxTemp, setMaxTemp] = useState(100);
+  const [isMinEnabled, setIsMinEnabled] = useState(true);
+  const [isMaxEnabled, setIsMaxEnabled] = useState(false);
+
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
-
       <Stack.Screen
           name="HomeScreen"
           component={HomeScreen}
           options={{ title: 'Welcome', min:'foobar'}}
-        />
-
-        
+        />  
       <Stack.Screen 
           name="SettingsScreen" 
           component={SettingsScreen} 
         />
-
-
       </Stack.Navigator>
     </NavigationContainer>
   )
